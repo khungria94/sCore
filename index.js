f567837868dba872ad13fadd41ddd1af0a299f5a
LeaderGrev <mikhail.klimentov@gmail.com>
2015-12-14 11:25:23 -0500

    Updated visual functionality on search
    - Added “Search” and “Hide buttons that expand and retract that Search
    panel within the sidebar div.

---
+++ index.js
@@ -0,0 +1,14 @@
+var token = "BQC69neuEcV4ot3esta2gorUBY742aYoCwyZgS9L4LADpKrWaiGPwYuEyX5";
+var client_id = "9ed7472797b146d1bc5cfaa17f19a6bf";
+var client_secret = "695b5b3e9db24f23bd71fcb8c2f85e8c";
+
+function expand() {
+	$('#results').slideDown();
+	$('#playlist').animate({height: '50%'});
+}
+
+function hide() {
+	$('#results').slideUp(); 
+	$('#playlist').animate({height: '95%'});
+}
+
