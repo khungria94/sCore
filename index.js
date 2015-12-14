06f1d97cb232328924722a77373ea1b2b348fcb9
Kenneth Hungria <k.hungria94@gmail.com>
2015-12-14 13:26:43 -0500

    Generated my token to have the right scopes on the spotify console, need to figure out how to do it with user authorization

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var token = "BQB2zEnsRSkdSAXGQlgl1kph80tZ2Xb60CtkbVokMdfsI8eqFv0aHF2Lpwthd-2E";
+var KennethToken = "BQAtw6BmPEv_baucW-GY-84l_ZDQ3PLsQ7jDvFLxIZiJoud9sW7ZbowcefQPexpvdTmcEbBD3LMWcGALg4WWWV4eRMTsR51JGOkA5_EN0lRHOBbXeybk76yAQ5FylOE8lUTUaQWMySrHTojP_Ga1T3H5xGq0iG6GIw6HHYI";
 var client_id = "9ed7472797b146d1bc5cfaa17f19a6bf";
 var client_secret = "695b5b3e9db24f23bd71fcb8c2f85e8c";
 var redirect_uri = "http://localhost"
-
+/*
 $.ajax({
 	url: 'https://api.spotify.com/v1/me',
 	data: {
@@ -13,7 +14,7 @@ $.ajax({
 		console.log(data.data.display_name);
 	}
 });
-
+*/
 function expand() {
 	$('#results').slideDown();
 	$('#playlist').animate({height: '50%'});
@@ -21,10 +22,10 @@ function expand() {
 	$.ajax({
 		url: 'https://api.spotify.com/v1/me',
 		headers: {
-			'Authorization': 'Bearer ' + token
+			'Authorization': 'Bearer ' + KennethToken
 		},
 		success: function(data) {
-			console.log(data.data.birthday);
+			console.log(data.birthdate);
 		}
 	});
 
