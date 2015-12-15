13098eff93bcc04fd56a08d312d5cba8e879cc86
LeaderGrev <mikhail.klimentov@gmail.com>
2015-12-15 13:23:41 -0500

    Results display
    - Search results (kind of goofy right now) are displayed in the result
    bar.

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-var Mikhail_token = "BQD8Zq9M1GYGD9eLMiS6zMpjhFEJV36YraM6-2VTq8DHKpJEHU7tK1PAZcAtGrhVafXU25ySeiSQ6YbRF0Kw7Grd1qcZ1CK1z-lIEvQGAtwqUz0D2TvSrrdeLh3ma-hvg943ILyOzz1fC4nYqRpcPluN4PeKAnjsr1UTf6KIQej-3TRF9o3f2opEAj2gOh50NnapZ1D69ctT3iqx6wget5DTgIMPC1L0Pv4-lBBFWFvzkzph2-cNXGZ6iJhGz1BiEfuc-qKVkeY977xrnHOb8pkEgu-gSrxjyZvuJdl3A9rl2fo";
-var Kenneth_token = "BQAGsjQM2yn6Kva-Mu2nbriDqISi2b_Y8bjtxeINLGkHXD5BlcGpOrhZypIdN9hoR422h0sHsdRiLMZrX0-L3nQLonTZYGetZzYgPm4P9VpS2Zane6-uw78fwycFui08_gJrrZ7tu_QMRRrx2Oulce0XEM7eWRU_ish37x8";
+// Spotify API
+var Mikhail_token = "BQAWq52LPA-hVh5tkhCFfR6hGfH1LMoVENR0NUBrmj4_WNr9n3AtOWEAaEsMsg96_QNyXk9e4xzMkIvaL6IwvbxufrXpZe9o9gQevb3fC7NlmSvg-e8KJIME35NYYK1-uxCBHlmYprxVxr_2bv_ASv0afJR7aIpp1wP27QMn-FcUbxGJm7jxiaiYARJjHKux6INztB6Cx3RDZcU-LcJR1bAc_5FjCy8wvPXiiBXpghe0BLlwUHKKCXP2TFo7s2A0whBwwkF7vJOq0YgCVs8-18eyhVyzTIvmTtL6zeptCDWruf8";
+var Kenneth_token = "BQAtw6BmPEv_baucW-GY-84l_ZDQ3PLsQ7jDvFLxIZiJoud9sW7ZbowcefQPexpvdTmcEbBD3LMWcGALg4WWWV4eRMTsR51JGOkA5_EN0lRHOBbXeybk76yAQ5FylOE8lUTUaQWMySrHTojP_Ga1T3H5xGq0iG6GIw6HHYI";
 var client_id = "9ed7472797b146d1bc5cfaa17f19a6bf";
 var client_secret = "695b5b3e9db24f23bd71fcb8c2f85e8c";
+
+// Echonest API
 var enkey = 'FIWW1FAOTC35KFT7L';
+
+// Universal
 var redirect_uri = "http://localhost";
 
-/*
-$.ajax({
-	url: 'https://api.spotify.com/v1/me',
-	data: {
-		'Authorization': token
-	},
-	success: function(data) {
-		console.log("auth");
-		console.log(data.data.display_name);
-	}
-});
-*/
-function expand() {
+
+function expand(que) {
 	$('#results').slideDown();
 	$('#playlist').animate({height: '50%'});
-
-	var x = document.getElementsByName("sname")[0].value;
-	search(x);
-/*
-	$.ajax({
-		url: 'https://api.spotify.com/v1/me',
-		headers: {
-			'Authorization': 'Bearer ' + Kenneth_token
-		},
-		success: function(data) {
-			console.log(data.birthdate);
-		}
-	});*/
-
-
+	search(que);
 }
 
 function hide() {
@@ -46,15 +26,17 @@ function search(q) {
 	$.ajax({
 		url: 'https://api.spotify.com/v1/search',
 		data: {
-			q: q,
-			type: 'track,artist',
+			q: "track:" + q,
+			type: 'track',
 			market: 'US'
 		},
-		success: function ENSearchSuccess(data, status, jqxhr){
-			console.log(data);
+		success: function(data){
+			for (var i = 0; i < data.tracks.items.length; i++) {
+				$(".results_display").append("<tr><td>" + data.tracks.items[i].name + "</td><tr>")
+			}
 		},
-		error: function ENSearchErr(jqxhr, status, err){
-			console.log(jqxhr, status, err);
+		error: function(data){
+			console.log(data);
 		}
 	});
 }
